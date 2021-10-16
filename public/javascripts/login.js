ed9a6df0913d140dd697eba2eb5b7b5de4a67607
Andrés Pérez <juanandresperezpt@gmail.com>
2021-10-15 20:49:23 -0500

    Subiendo proyecto

---
+++ public/javascripts/login.js
@@ -0,0 +1,15 @@
+function validar_captcha(captcha){
+    if(captcha==5){
+      return true;
+    }else{
+      return false;
+    }
+}
+
+function iniciar_sesion(usuario, password, captcha){
+
+  if(validar_captcha(captcha)){
+
+  }
+
+}
